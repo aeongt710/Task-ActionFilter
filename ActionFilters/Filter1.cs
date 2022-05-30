ae8256c3f66ba8937ceb88edfbfe78f5bc0e9a42
Muhammad Ahmad <unknowndonlion@gmail.com>
2022-05-30 10:59:29 +0500

    Add project files.

---
+++ ActionFilters/Filter1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Diagnostics;
+
+namespace ActionFilter1.ActionFilters
+{
+    public class Filter1 : IActionFilter
+    {
+        Stopwatch sw = new Stopwatch();
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+            context.HttpContext.Response.Headers.Add("Execution-Time", sw.Elapsed.ToString());
+            context.HttpContext.Response.Headers.Add("HTTP-Method", context.HttpContext.Request.Method);
+            context.HttpContext.Response.Headers.Add("HTTP-Scheme", context.HttpContext.Request.Scheme);
+            context.HttpContext.Response.Headers.Add("Action-Name", context.ActionDescriptor.DisplayName);
+            context.HttpContext.Response.Headers.Add("Host", context.HttpContext.Request.Host.ToString());
+            context.HttpContext.Response.Headers.Add("Port", context.HttpContext.Request.Host.Port.ToString());
+            context.HttpContext.Response.Headers.Add("Server-Time", DateTime.Now.ToString());
+
+            sw.Stop();
+
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            sw.Start();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using ActionFilter1.ActionFilters;
+using ActionFilter1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionFilter1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [ServiceFilter(typeof(Filter1))]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [ServiceFilter(typeof(Filter1))]
+        public IActionResult Index2()
+        {
+            return Ok("Eerything Ok");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
